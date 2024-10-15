506d8fef6197f7a39409084db1bbcbc68f2e8ed0
Hayden <HaydenJenkins>
2024-10-15 13:28:09 -0400

    added drawer nav (nested) and fixed unwanted event generation in agenda.

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { useState, useRef } from 'react';
+import { Button, Image, TouchableOpacity } from 'react-native';
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import { GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler';
 
 import { Agenda } from 'react-native-calendars';
-import { Avatar, Card } from 'react-native-paper';
+import { Avatar, Card, IconButton, MD3Colors } from 'react-native-paper';
 
 
 const Row = ({ children }) => (
@@ -18,16 +20,55 @@ const timeToString = (time) => {
   return date.toISOString().split('T')[0];
 }
 
-const Stack =  createStackNavigator();
+const LogoTitle = () => {
+  return (
+    <Image
+      style={{ width: 25, height: 25 }}
+      source={require('./assets/favicon.png')}
+    />
+  );
+}
+
+const Stack = createStackNavigator();
 const StackNavigator = () => (
   <Stack.Navigator>
-    <Stack.Screen name='HomeScreen' component={HomeScreen} />
-    <Stack.Screen name='CalendarScreen' component={CalendarScreen} />
+    <Stack.Screen name='Drawer' component={DrawerNavigator} options={{ headerShown: false, title: 'Home' }} />
+    <Stack.Screen name='Calendar' component={CalendarScreen} options={{ headerBackTitle: 'Back' }} />
   </Stack.Navigator>
 );
 
+const Drawer = createDrawerNavigator();
+const DrawerNavigator = () => (
+  <Drawer.Navigator screenOptions={{
+    drawerStyle: { backgroundColor: '#c6cbef', width: 200 },
+    gestureEnabled: true,
+    swipeEdgeWidth: 1000,
+  }} >
+    <Drawer.Screen name='Home' component={HomeScreen} options={{ headerTitle: () => <LogoTitle />, drawerLabel: () => <LogoTitle /> }} />
+    <Drawer.Screen name='Account' component={AccountScreen} />
+    {/* <Drawer.Screen name='Calendar' component={CalendarScreen} /> */}
+  </Drawer.Navigator>
+);
+
+const AccountScreen = ({ navigation }) => {
+  return (
+    <View style={{ 
+      flex: 1, 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      }}>
+      <Button onPress={() => navigation.goBack()} title='Go back home' />
+    </View>
+  );
+};
+
 const CalendarScreen = () => {
+  const today = new Date();
   const [items, setItems] = useState({});
+  const [date, setDate] = useState({
+    timestamp: today.getTime(),
+    dateString: today.toISOString().split('T')[0],
+  });
 
   const loadItems = (day) => {
     setTimeout(() => {
@@ -47,11 +88,7 @@ const CalendarScreen = () => {
         //   });
         // }
       
-        items[strTime].push({
-          name: 'test for ' + strTime,
-          height: 50,
-          day: strTime
-        });
+        
       }
     
       const newItems = {};
@@ -62,6 +99,18 @@ const CalendarScreen = () => {
     }, 1000);
   };
   
+  const createItem = () => {
+    const time = date.timestamp
+    const strTime = timeToString(time);
+
+    items[strTime].push({
+      name: 'test for ' + strTime,
+      height: 50,
+      day: strTime
+    });
+
+    loadItems(date);
+  };
   
   const renderItem = (item) => {
     return (
@@ -80,6 +129,12 @@ const CalendarScreen = () => {
                 alignItems: 'center',
               }}>
               <Text>{item.name}</Text>
+              <IconButton
+                icon="camera"
+                iconColor={MD3Colors.error50}
+                size={20}
+                onPress={() => console.log('Pressed')}
+              />
               <Avatar.Text label='H' />
             </View>
           </Card.Content>
@@ -97,11 +152,15 @@ const CalendarScreen = () => {
       <StatusBar />
       <View style={{ flex: 1 }}>
         <Agenda
-            items={items}
-            loadItemsForMonth={loadItems}
-            //selected={'2024-09-23'}
-            renderItem={renderItem}
-          />
+          items={items}
+          loadItemsForMonth={loadItems}
+          //selected={'2024-09-23'}
+          renderItem={renderItem}
+          onDayPress={(day) => setDate(day)}
+        />
+        <View style={{ padding: 20 }}>
+          <Button title="Add Event" onPress={createItem} />
+        </View>
       </View>
     </SafeAreaView>
   )
@@ -115,7 +174,7 @@ const HomeScreen = ({ navigation }) => (
         </View> */}
         <TouchableOpacity 
           style={styles.calendarCell}
-          onPress={() => navigation.navigate('CalendarScreen')}>
+          onPress={() => navigation.navigate('Calendar')}>
           <Text>calendar preview (that day as preview)</Text>
         </TouchableOpacity>
       </View>
@@ -140,13 +199,33 @@ const HomeScreen = ({ navigation }) => (
     </SafeAreaView>
 );
 
+const MainScreen = () => {
+  // const navigationRef = useRef(null);
+
+  // const onGestureEvent = (event) => {
+  //   if (event.nativeEvent.translationX > 500) {
+  //     navigationRef.current?.dispatch(DrawerActions.openDrawer());
+  //   }
+  // };
+
+  return (
+    // <PanGestureHandler onGestureEvent={onGestureEvent} >
+    //   <DrawerNavigator />
+    // </PanGestureHandler>
+    <StackNavigator />
+  );
+}
+
+
 
 export default function App() {
 
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <MainScreen />
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
